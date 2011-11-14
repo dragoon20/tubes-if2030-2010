cac14cb48039d477567a5d25551ed6e14dcdd244
fernandojordan.92 <fernandojordan.92@gmail.com>
2011-11-14 06:10:03 +0000

    tambah .c

---
+++ listpertemanan.c
@@ -0,0 +1,54 @@
+
+#include "listpertemanan.h"
+
+int NUser (List L)
+{
+}
+
+int NFriend (List L, infotype X)
+{
+}
+
+void AddFriend (List L, infotype X, infotype added)
+{
+}
+
+void DeleteFriend (List L, infotype X, infotype deleted)
+{
+}
+
+void AddUser (List L, infotype X)
+{
+}
+
+void RemoveUser (List L, infotype X)
+{
+}
+
+void ModifyUser (List L, infotype X)
+{
+}
+
+void Save (List L, char namafile[50])
+{
+}
+
+void Load (List L, char namafile[50])
+{
+}
+
+void SortAsc ()
+{
+}
+
+void SortDesc ()
+{
+}
+
+int IsTeman (List L, infotype X, infotype temanX)
+{
+}
+
+bool IsSame (List L, infotype X, infotype temanX, int parameter)
+{
+}
